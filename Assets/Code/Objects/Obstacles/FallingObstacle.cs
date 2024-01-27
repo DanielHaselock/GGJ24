ca17f7d1ffc0cd30b315dfa930ca06d53d0cb5d6
jordan-vision <byjordan11@gmail.com>
2024-01-27 11:31:24 -0500

    Falling obstacle

---
+++ Assets/Code/Objects/Obstacles/FallingObstacle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObstacle : GenericObstacle
+{
+    bool m_hasFallen;
+
+    protected override void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag.Equals("Solid"))
+            m_hasFallen = true;
+
+        if (!m_hasFallen)
+            base.OnCollisionEnter2D(collision);
+    }
+}
