1577a5154de190fc6d48eb768ad721fe6ddbb477
jordan-vision <byjordan11@gmail.com>
2024-01-28 13:42:23 -0500

    clown randomizer

--- Assets/Code/Player/PlayerCustomization.cs
+++ Assets/Code/Player/PlayerCustomization.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class PlayerCustomization : MonoBehaviour
 {
+    [SerializeField] RuntimeAnimatorController[] m_clownAnimators;
     [SerializeField] Texture2D[] m_palettes;
     [SerializeField] SpriteRenderer m_spriteRenderer;
 
     public void Randomize()
     {
+        // Randomize clown
+        int random = Random.Range(0, m_clownAnimators.Length);
+        GetComponent<Animator>().runtimeAnimatorController = m_clownAnimators[random];
+
+        // Randomize color
         Material material = m_spriteRenderer.material;
         m_spriteRenderer.enabled = false;
         m_spriteRenderer.material = null;
 
-        int random = Random.Range(0, m_palettes.Length);
+        random = Random.Range(0, m_palettes.Length);
         material.SetTexture("_GradientTexture", m_palettes[random]);
 
         m_spriteRenderer.materials = new Material[1] { material };
--- readme.txt
+++ readme.txt
@@ -2,16 +2,14 @@ TODO -- PRIORITIES
 
 ART
     Title screen UI
-    Level win / level lose screens
     Backgrounds
 PROGRAMMING
     Figure out proper build settings
 LEVEL DESIGN
     Add tileset to levels
-AUDIO
+AUDIO (implemetation)
     Boing!
     Anchor hit floor
-    Anchor hit skull
     Balloon pop
     Coin get
 
@@ -23,8 +21,6 @@ ART
     Credits screen
     Audience
 PROGRAMMING
-    Randomize player
-    UI control with keyboard
     High score save file
     Orb
     Gasp Sound effect
