e205c0283ab31a309195ce9a45168fae0687b2ed
KV-00 <77709554+KV-00@users.noreply.github.com>
2025-05-14 17:10:06 -0400

    Added balloon randomization
    - renamed balloon's script
    - removed obstacles script folder, everything is now in the objects folder
    - modified checkpoint 1 level
    - tweaked balloon sprite to stop cutting off at top
    - added in balloon randomization

--- Assets/Code/Objects/Balloon.cs
+++ Assets/Code/Objects/Balloon.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RisingObstacle : GenericObstacle
+public class Balloon : GenericObstacle
 {
     private Rigidbody2D m_rb;
     private BoxCollider2D collider;
+    private SpriteRenderer m_sr;
+    public Material[] BalloonMaterials;
     [SerializeField] private GameObject confetti;
 
     protected override void Start()
     {
         base.Start();
         m_rb = GetComponent<Rigidbody2D>();
+        m_sr = GetComponent<SpriteRenderer>();
+        
+        int randomIndex = Random.Range(0, BalloonMaterials.Length);
+        m_sr.material = BalloonMaterials[randomIndex];
     }
 
     protected override void OnCollisionEnter2D(Collision2D collision)
