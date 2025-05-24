03f972fa6bea6496da810a912efc08a4307a7fab
KV-00 <77709554+KV-00@users.noreply.github.com>
2025-05-24 15:18:32 -0400

    added curtain animations
    - added left curtain animations
    - added right curtain animations
    - added opening aniamtion trigger to loader script

--- Assets/Code/Managers/AdamScripts/Loader.cs
+++ Assets/Code/Managers/AdamScripts/Loader.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 /// </summary>
 public class Loader : MonoBehaviour
 {
-
+    [SerializeField] private GameObject LeftCurtain;
+    [SerializeField] private GameObject RightCurtain;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         DontDestroyOnLoad(gameObject);
+        LeftCurtain.GetComponent<Animator>().SetTrigger("open");
+        RightCurtain.GetComponent<Animator>().SetTrigger("open");
     }
 
     // Update is called once per frame
