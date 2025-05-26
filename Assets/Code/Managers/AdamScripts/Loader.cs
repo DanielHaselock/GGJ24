e7dd346a42a104b5895dd6a4e915e3777aabef56
Adam Farahat <farahatadam@gmail.com>
2025-05-26 14:07:24 -0400

    Implement curtain loading for newMainMenu, Lobby, and Scoreboard
    Currently will not work in the sample scene since there is no curtains there yet. Currently, the curtains do not work correctly in the scoreboard and due to the way the level scenes are done the curtains appear really small  when put in.

--- Assets/Code/Managers/AdamScripts/Loader.cs
+++ Assets/Code/Managers/AdamScripts/Loader.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 /// <summary>
 ///  MonoBehaviour script that manages scene loading.
@@ -8,12 +9,14 @@ public class Loader : MonoBehaviour
 {
     [SerializeField] private GameObject LeftCurtain;
     [SerializeField] private GameObject RightCurtain;
+
+    [SerializeField] private float curtainClosedDelay = 2.0f; // Delay before opening curtains
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        InitializeCurtains();
         DontDestroyOnLoad(gameObject);
-        LeftCurtain.GetComponent<Animator>().SetTrigger("open");
-        RightCurtain.GetComponent<Animator>().SetTrigger("open");
+
     }
 
     // Update is called once per frame
@@ -21,9 +24,38 @@ public class Loader : MonoBehaviour
     {
 
     }
-    
+
     public void LoadScene(string sceneName)
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+
+        StartCoroutine(LoadSceneWithCurtains(sceneName));
+    }
+
+    IEnumerator LoadSceneWithCurtains(string sceneName)
+    {
+        LeftCurtain.GetComponent<Animator>().SetTrigger("close");
+        RightCurtain.GetComponent<Animator>().SetTrigger("close");
+        yield return new WaitForSeconds(curtainClosedDelay);
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName);
+        while (!UnityEngine.SceneManagement.SceneManager.GetSceneByName(sceneName).isLoaded)
+        {
+            yield return null; // Wait until the scene is loaded
+        }
+        InitializeCurtains();
+    }
+
+    public void InitializeCurtains()
+    {
+        LeftCurtain = GameObject.Find("LeftCurtain");
+        RightCurtain = GameObject.Find("RightCurtain");
+        if (LeftCurtain == null || RightCurtain == null)
+        {
+            Debug.LogError("Curtains not found in the scene. Please ensure they are present.");
+        }
+        else
+        {
+            LeftCurtain.GetComponent<Animator>().SetTrigger("open");
+            RightCurtain.GetComponent<Animator>().SetTrigger("open");
+        }
     }
 }
