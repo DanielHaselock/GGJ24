e821f366d5ea7e2b9d96294a9d765c177b7594f8
KV-00 <77709554+KV-00@users.noreply.github.com>
2025-05-26 16:52:48 -0400

    fixed scoreboard + misc.
    - fixed scoreboard curtains
    - made scoreboard closer to player into mockup
    - tweaked background palette 8
    - moved quit button and version buttons back in front
    - started work on main menu states to hide/show buttons, lobby and other items all on one scene

--- Assets/Code/Managers/AdamScripts/MainMenu.cs
+++ Assets/Code/Managers/AdamScripts/MainMenu.cs
@@ -3,6 +3,39 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject GameTitle;
+    [SerializeField] private GameObject Buttons;
+    [SerializeField] private GameObject Prompt;
+    // 0 = on boot up, 1 = main menu, 2 = lobby, 3 = credits, 4 = start game
+    [SerializeField] private int MenuState = 0;
+
+    void Update()
+    {
+        MenuStateManager();
+    }
+
+    public void MenuStateManager()
+    {
+        switch (MenuState)
+        {
+        case 2:
+            GameTitle.GetComponent<Animator>().SetTrigger("title_menu");
+            Prompt.SetActive(false);
+            break;
+        case 1:
+            GameTitle.GetComponent<Animator>().SetTrigger("title_menu");
+            Prompt.SetActive(true);
+            Prompt.transform.localPosition = new Vector3(0, -32, 0);
+            break;
+        case 0:
+            GameTitle.GetComponent<Animator>().SetTrigger("title_start");
+            Prompt.transform.localPosition = new Vector3(0, -52, 0);
+            break;
+        default:
+            break;
+        }
+    }
+
     public void GoToScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
