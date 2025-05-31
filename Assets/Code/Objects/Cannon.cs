adab58273d532795d3f077275d7112a61eea7d61
KV-00 <77709554+KV-00@users.noreply.github.com>
2025-05-31 13:07:49 -0400

    start of cannon rework
    - added sprites to spawner object
    - renamed spawner to "cannon"
    - gave cannon a proper spawn point at the end of its barrel
    - start (but not finished) reworking cannon code to shoot out objects

--- Assets/Code/Objects/Cannon.cs
+++ Assets/Code/Objects/Cannon.cs
@@ -2,19 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectSpawner : MonoBehaviour
+public class Cannon : MonoBehaviour
 {
     // Start is called before the first frame update
 
     [SerializeField] private float SpawnRateUpper = 2f;
     [SerializeField] private float SpawnRateLower = 5f;
     private float Timer = 0f;
+    [SerializeField] private Animator m_animator;
+    [SerializeField] private Transform SpawnPoint;
 
     private float SpawnRate;
+    [SerializeField] private float SpawnedObjectSpeed = 10f;
 
-    [SerializeField] private GameObject Spawned;
+    [SerializeField] private GameObject SpawnedObject;
+
     void Start()
     {
+        m_animator = GetComponent<Animator>();
+
         SpawnRate = Random.Range(SpawnRateLower, SpawnRateUpper);
         Timer = SpawnRate;
     }
@@ -25,9 +31,14 @@ public class ObjectSpawner : MonoBehaviour
         Timer -= Time.deltaTime;
         if(Timer <= 0)
         {
-            Instantiate(Spawned, gameObject.transform.position, Quaternion.identity);
+            Instantiate(SpawnedObject, SpawnPoint.transform.position, Quaternion.identity);
+
+            Rigidbody2D SpawnedObjectRb = SpawnedObject.GetComponent<Rigidbody2D>();
+            SpawnedObjectRb.linearVelocity = SpawnPoint.forward * SpawnedObjectSpeed;
+
             SpawnRate = Random.Range(SpawnRateLower, SpawnRateUpper);
             Timer = SpawnRate;
+            m_animator.SetTrigger("shoot");
         }
 
     }
