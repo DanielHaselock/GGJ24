d030fbf90866a04e5ad86c537bdec22373aa234b
Daniel Haselock <daniel.haselock@gmail.com>
2025-06-12 19:27:40 -0400

    Fix animation on death

--- Assets/Code/Managers/GameManager/GameManager.cs
+++ Assets/Code/Managers/GameManager/GameManager.cs
@@ -159,12 +159,6 @@ public class GameManager : MonoBehaviour
         // }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void QuitGame()
     {
         Debug.Log("Quit Game");
