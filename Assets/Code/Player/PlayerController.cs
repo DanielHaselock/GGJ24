b7e635445ef645846a7aa070d37fce0e1b058231
Daniel Haselock <daniel.haselock@gmail.com>
2025-06-18 23:33:04 -0400

    fix for singleplayer

--- Assets/Code/Player/PlayerController.cs
+++ Assets/Code/Player/PlayerController.cs
@@ -201,7 +201,10 @@ public class PlayerController : MonoBehaviour
             //}
             if (context.started)
             {
-                GameManager.Instance.GoToLobby();
+                if(PlayerManager.Instance.players.Count > 1)
+                    GameManager.Instance.GoToLobby();
+                else
+                    GameManager.Instance.GoToMainMenu();
             }
         }
     }
