ca0d3dac7c0c6f6bb440a2d24df0d97a202754b3
Daniel Haselock <daniel.haselock@gmail.com>
2025-06-30 15:19:31 -0400

    fixed not adding scores race Level bug

--- Assets/Code/Managers/LevelManager/RaceLevelManager.cs
+++ Assets/Code/Managers/LevelManager/RaceLevelManager.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,26 +9,45 @@ public class RaceLevelManager : BaseLevelManager
     [SerializeField] private int[] scores = new int[4];
 
     private List<GameObject> players = new List<GameObject>();
+    private bool hasAddedScores = false;
 
     protected override void Start()
     {
         base.Start();
+        hasAddedScores = false;
     }
 
     public void OnPlayerFinished(GameObject player)
     {
         if (!players.Find(p => p == player))
             players.Add(player);
-        else return;
+        else 
+            return;
+
         if (players.Count == PlayerManager.Instance.players.Count)
         {
-            for (int i = 0; i < players.Count; ++i)
-            {
-                PlayerManager.Instance.players.Find(p => p.PlayerIndex == players[i].GetComponent<PlayerController>().PlayerIndex).AddScore(scores[i]);
-            }
-
+            addScores();
             this.OnRoundEndEarly();
         }
+    }
+
+    public override void OnRoundEnd()
+    {
+        addScores();
+        GameManager.Instance.EndRoundCheck();
+    }
+
+    private void addScores()
+    {
+        if (hasAddedScores)
+            return;
+
+        for (int i = 0; i < players.Count; ++i)
+        {
+            PlayerManager.Instance.players.Find(p => p.PlayerIndex == players[i].GetComponent<PlayerController>().PlayerIndex).AddScore(scores[i]);
+        }
+
+        hasAddedScores = true;
     }
 
 }
